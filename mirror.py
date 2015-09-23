cb3282c259e8191088b94cdf5ded2bb695d91c35
nrtkbb <nrtkbb@gmail.com>
2015-09-23 12:25:38 +0900

    - コアな実装が完了した - envelopeが0になってしまってたり - コメントが足りてなかったり - 関数化してなかったり - 機能が足りなかったりしてるけど、一旦コミット

--- mirror.py
+++ mirror.py
@@ -1,122 +1,161 @@
 #! -*- coding: utf-8 -*-
 import pymel.core as pm
-import re
-from itertools import chain
 
 
-def divide_select_list(selected):
-    """
-    :param list of pm.PyNode selected: selection list
-    :rtype: (list of pm.nt.Cluster, pm.nt.Mesh)
-    """
-    if 2 > len(selected):
-        raise Exception(u'Please select clusters and mesh.')
-
-    clusters = pm.listConnections \
-        (selected[0:-1], destination=True, type=u'cluster')
-    if 0 >= len(clusters):
-        raise Exception(u'Please select clusters and mesh.')
-
-    paste_mesh = selected[-1].getShape()
-    if type(paste_mesh) is not pm.nodetypes.Mesh:
-        raise Exception(u'Finally select the mesh.')
-
-    return clusters, paste_mesh
+def vtx2pointsDict(vtx):
+    return {v: v.getPosition(space='world') for v in vtx}
 
 
-class ClusterCopy(object):
-    re_node_name = re.compile(r"^[^\.]+")
-
-    def __init__(self, cluster, mesh):
-        self.cluster = cluster
-        self.mesh = mesh
-        self.members = self._get_members()
-
-    def select_same_members(self, dst_mesh):
-        pm.select(self._get_renamed_members(dst_mesh))
-
-    def copy(self, dst_cluster, dst_mesh):
-        self._attr_copy(dst_cluster)
-        self._weight_copy(dst_cluster, dst_mesh)
-
-    def _get_members(self):
-        res = []
-        for s in pm.listSets(object=self.cluster):
-            res.extend(s.members())
-        return res
-
-    def _get_renamed_members(self, dst_mesh):
-        dst_name = dst_mesh.name()
-        res = []
-        for vtx in chain.from_iterable(self.members):
-            renamed_vtx = ClusterCopy.re_node_name.sub(dst_name, vtx.name())
-            res.append(renamed_vtx)
-        return res
-
-    def _attr_copy(self, dst_cluster):
-        src = self.cluster
-        dst_cluster.attr('relative') \
-            .set(src.attr('relative').get())
-        dst_cluster.attr('usePartialResolution') \
-            .set(src.attr('usePartialResolution').get())
-        dst_cluster.attr('angleInterpolation') \
-            .set(src.attr('angleInterpolation').get())
-        dst_cluster.attr('percentResolution') \
-            .set(src.attr('percentResolution').get())
-        dst_cluster.setEnvelope(src.getEnvelope())
-
-    def _weight_copy(self, dst_cluster, dst_mesh):
-        weights = pm.percent(self.cluster, self.mesh, query=True, value=True)
-        for i, weight in enumerate(weights):
-            pm.percent(dst_cluster, dst_mesh.vtx[i], value=weight)
+def pt2pointsDict(pt):
+    xform = pm.xform
+    point = pm.dt.Point
+    return {p: point(xform(p, query=True, translation=True, worldSpace=True))
+            for p in pt}
 
 
-def mesh2pointsDict(mesh):
-    return {v: v.getPosition(space='world') for v in mesh.vtx}
+def distanceMin(turn_x_point, points):
+    return min(points, key=lambda k: points[k].distanceTo(turn_x_point))
 
 
-def distanceMin(turn_x_point, mesh_points):
-    return min(mesh_points, key=lambda k: mesh_points[k].distanceTo(turn_x_point))
-
-
-def mirror_copy(cluster, mesh):
-    cc = ClusterCopy(cluster, mesh)
-    mesh_points = mesh2pointsDict(mesh)
-    turn_x_weights = {}
-    for vtx in chain.from_iterable(cc.members):
-        cl_point = vtx.getPosition(space='world')
-        turn_x_point = \
-            pm.datatypes.Point(cl_point.x * -1, cl_point.y, cl_point.z)
-        turn_x_vtx = \
-            distanceMin(turn_x_point, mesh_points)
-        turn_x_weights[turn_x_vtx] = \
-            pm.percent(cluster, vtx, query=True, value=True)[0]
-    pm.select(turn_x_weights.keys())
-    new_cluster, _ = pm.cluster()
-    cc.copy(new_cluster, mesh)
-    for vtx in turn_x_weights:
-        pm.percent(new_cluster, vtx, value=turn_x_weights[vtx])
-
-
-def mirror_mesh():
+def mirror_cluster_on_lattice():
     selected = pm.selected()
-    clusters, paste_mesh = divide_select_list(selected)
-    for cluster in clusters:
-        copy_mesh = cluster.getGeometry()
-        if 1 < len(copy_mesh):
-            print(u'Not support multi mesh cluster({}). skipped.'.format(cluster.name))
+    # 選択しているノードの子孫から clusterHandle をリストアップする
+    target_cluster_handles = pm.ls(selection=True,
+                                   dagObjects=True,
+                                   type=u'clusterHandle')
+
+    # clusterHandle を選択していなければ終了
+    assert 0 != len(target_cluster_handles), \
+        u'Not find clusterHandle from selection, ' \
+        u'Please select one or more clusterHandle'
+
+    # clusterHandle のコネクションを追って cluster をリストアップする
+    target_clusters = pm.listConnections(target_cluster_handles,
+                                         destination=True,
+                                         type=u'cluster')
+
+    # いったん無効化するデフォーマのセットを格納する変数を宣言
+    deformers = set()
+
+    for target_cluster in target_clusters:
+        # cluster のデフォーム対象を getGeometry で取得: 名前の配列が返る
+        output_geometries = target_cluster.getGeometry()
+
+        # type を調べるために一度 PyNode にする
+        output_geometries = [pm.PyNode(g) for g in output_geometries]
+
+        # フィルタリングする前の数を保存
+        output_num = len(output_geometries)
+
+        # デフォーム対象を mesh か lattice だけにする
+        output_geometries = filter(lambda g:
+                                   pm.nodetypes.Mesh == type(g) or
+                                   pm.nodetypes.Lattice == type(g),
+                                   output_geometries)
+
+        # mesh か lattice 以外にもセットされてる cluster ならスキップする
+        if output_num is not len(output_geometries):
             continue
 
-        try:
-            copy_mesh = pm.nt.Mesh(copy_mesh[0])
-        except TypeError:
-            print(u'Not support type not mesh cluster({}). skipped.'.format(cluster.name))
+        # 中身が無くなってもスキップする
+        if 0 == len(output_geometries):
             continue
 
-        if copy_mesh.name() == paste_mesh.name():
-            mirror_copy(cluster, paste_mesh)
-        else:
-            cc = ClusterCopy(cluster, copy_mesh)
-            cc.select_same_members(paste_mesh)
-            new_cluster, _ = pm.cluster()
-            cc.copy(new_cluster, paste_mesh)
+        # デフォーム対象のヒストリをリストアップする
+        histories = pm.listHistory(output_geometries)
+
+        # 継承している nodeType を調べて geometryFilter が含まれていたらデフォーマと確定
+        [deformers.add(d)
+         for d in histories
+         if u'geometryFilter' in pm.nodeType(d, inherited=True)]
+
+    # あとで復元する envelope を保存。デフォーマをキーにしつつ envelope を保存する
+    envelopes = {d:d.getEnvelope() for d in deformers}
+
+    # デフォーマを無効化し元の形状にする
+    [d.setEnvelope(0) for d in deformers]
+
+    for target_cluster in target_clusters:
+        # cluster のデフォーム対象を getGeometry で取得: 名前の配列が返る
+        output_geometries = target_cluster.getGeometry()
+
+        # type を調べるために一度 PyNode にする
+        output_geometries = [pm.PyNode(g) for g in output_geometries]
+
+        # フィルタリングする前の数を保存
+        output_num = len(output_geometries)
+
+        # デフォーム対象を mesh か lattice だけにする
+        output_geometries = filter(lambda g:
+                                   pm.nodetypes.Mesh == type(g) or
+                                   pm.nodetypes.Lattice == type(g),
+                                   output_geometries)
+
+        # mesh か lattice 以外にもセットされてる cluster ならスキップする
+        if output_num is not len(output_geometries):
+            continue
+
+        # 中身が無くなってもスキップする
+        if 0 == len(output_geometries):
+            continue
+
+        members = []
+        [members.extend(x) for x in pm.listSets(object=target_cluster)]
+        members = pm.ls(members, flatten=True)
+        member_dict = {}
+        all_points_dict = {}
+        for member in members:
+            node = member.node()
+            if node in member_dict:
+                member_dict[node].append(member)
+                continue
+
+            member_dict[node] = [member]
+            if pm.nt.Mesh == type(node):
+                all_points_dict[node] = vtx2pointsDict(node.vtx)
+            elif pm.nt.Lattice == type(node):
+                all_points_dict[node] = pt2pointsDict(node.pt)
+
+        weights = {}
+        for node in member_dict.iterkeys():
+            all_points = all_points_dict[node]
+            if pm.nt.Mesh == type(node):
+                for member in member_dict[node]:
+                    p = member.getPosition(space='world')
+                    p = pm.dt.Point(p.x * -1, p.y, p.z)
+                    v = distanceMin(p, all_points)
+                    weights[v] = pm.percent(target_cluster,
+                                            member,
+                                            query=True,
+                                            value=True)[0]
+            elif pm.nt.Lattice == type(node):
+                xform = pm.xform
+                point = pm.dt.Point
+                for member in member_dict[node]:
+                    p = xform(member,
+                              query=True,
+                              translation=True,
+                              worldSpace=True)
+                    p = point(p[0] * -1, p[1], p[2])
+                    v = distanceMin(p, all_points)
+                    weights[v] = pm.percent(target_cluster,
+                                            member,
+                                            query=True,
+                                            value=True)[0]
+        pm.select(weights.keys())
+        new_cluster, _ = pm.cluster()
+
+        new_cluster.attr('relative') \
+            .set(target_cluster.attr('relative').get())
+        new_cluster.attr('usePartialResolution') \
+            .set(target_cluster.attr('usePartialResolution').get())
+        new_cluster.attr('angleInterpolation') \
+            .set(target_cluster.attr('angleInterpolation').get())
+        new_cluster.attr('percentResolution') \
+            .set(target_cluster.attr('percentResolution').get())
+        new_cluster.setEnvelope(envelopes[target_cluster])
+
+    # envelope を復元
+    [d.setEnvelope(envelopes[d]) for d in envelopes.iterkeys()]
+
+    pm.select(selected)
